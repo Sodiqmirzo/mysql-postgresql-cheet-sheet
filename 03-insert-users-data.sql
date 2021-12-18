938874992188a7bd2c0d6d1d17ba7e81097896d0
Sodiqmirzo Sattorov <65369403+Sodiqmirzo@users.noreply.github.com>
2021-12-18 14:13:58 +0500

    insert data again

---
+++ 03-insert-users-data.sql
@@ -0,0 +1,9 @@
+--INSERT INTO users (yearly_salary,full_name,current_status) 
+--VALUES (19000,'Sodikmirzo Sattorov','self-employed');
+
+
+INSERT INTO users (yearly_salary,full_name,current_status) 
+VALUES (25000,'Javohir Jumayev','employed');
+
+INSERT INTO users (yearly_salary,full_name,current_status) 
+VALUES (0,'Nodirjon Toirov','unemployed');
