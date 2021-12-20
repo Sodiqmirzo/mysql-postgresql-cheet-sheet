f984f86aabfde9fdd3f29f7dd44ef0214a81d354
Sodiqmirzo Sattorov <65369403+Sodiqmirzo@users.noreply.github.com>
2021-12-20 19:20:20 +0500

    set auto increment id table

---
+++ 17-primary-key.sql
@@ -0,0 +1,34 @@
+DROP TABLE users;
+DROP TABLE employers;
+DROP TABLE conversations;
+
+-- CREATE TYPE employment_status AS ENUM ('employed','self-employed','unemployed');  --Postgresql
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    -- id INT PRIMARY KEY AUTO_INCREMENT, --Mysql
+    full_name VARCHAR(300) NOT NULL,
+    yearly_salary INT CHECK(yearly_salary > 0),
+    -- current_status ENUM('employed','self-employed','unemployed') 
+    -- Mysql
+    current_status employment_status
+);
+
+CREATE TABLE employers(
+    id SERIAL PRIMARY KEY,
+    -- id INT PRIMARY KEY AUTO_INCREMENT, --Mysql
+    company_name VARCHAR(300) NOT NULL,
+    company_address VARCHAR(300) NOT NULL,
+    yearly_revenue FLOAT CHECK(yearly_revenue > 0),
+    is_hiring BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE conversations(
+    id SERIAL PRIMARY KEY,
+    -- id INT PRIMARY KEY AUTO_INCREMENT, --Mysql
+    user_id INT,
+    employer_id INT,
+    message TEXT NOT NULL
+);
+
+-- 50
