12b0a146e03b9eb2fac8eea4a9c56ca1b3b7c94e
Sodiqmirzo Sattorov <65369403+Sodiqmirzo@users.noreply.github.com>
2022-01-26 09:15:35 +0500

    exercise solution

---
+++ 19-exercise-solution.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE online_shop;
+
+-- CREATE TABLE products (
+--     name VARCHAR(200),
+--     price NUMBER(10,2),
+--     description TEXT,
+--     amount_in_stock SMALLINT,
+--     image_path VARCHAR(500),
+-- );
+
+-- INSERT INTO products (name, price, description, amount_in_stock,image_path)
+-- VALUES ('A book',23.98,'Small book which is better',4,'uploads/products/product-1.jpg');
+-- 56
